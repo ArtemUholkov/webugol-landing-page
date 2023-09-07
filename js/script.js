fe6b14783026242fad86f7208f2e8e67dca095d8
Artem <artem.ugolkov@ukr.net>
2023-09-07 16:55:46 +0300

    header ready, need to check margins

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const popupSlide = document.querySelector('#popupSlide');
 const sliderItem = document.querySelectorAll('.our_requirements_content_item');
 const sliderPhoto = document.querySelector('.our_requirements_img');
 const body = document.body;
+const casualLogo = document.querySelector('.logo-white');
+const colorLogo = document.querySelector('.logo-color');
 
 burger.addEventListener('click', burgerHandler);
 popup.addEventListener('click', (e) => {
@@ -21,17 +23,30 @@ function burgerHandler(e) {
     popup.classList.add('close');
     body.classList.remove('noscroll');
     popupSlide.classList.add('slideout');
-
+    casualLogo.classList.remove('logo-hide');
+    colorLogo.classList.add('logo-hide');
+    setTimeout(() => {
+      casualLogo.classList.remove('logo-hide');
+      colorLogo.classList.add('logo-hide');
+      // colorLogo.classList.remove('logo-hide');
+      // casualLogo.classList.add('logo-hide');
+    }, 270);
     setTimeout(() => {
       popup.classList.remove('close');
       popup.classList.remove('open');
       popupSlide.classList.remove('slideout');
       popupSlide.classList.remove('slidein');
-    }, 300);
+    }, 270);
   } else {
     body.classList.add('noscroll');
     popup.classList.add('open');
     popupSlide.classList.add('slidein');
+    setTimeout(() => {
+      //   casualLogo.classList.remove('logo-hide');
+      // colorLogo.classList.add('logo-hide');
+      colorLogo.classList.remove('logo-hide');
+      casualLogo.classList.add('logo-hide');
+    }, 270);
   }
   burger.classList.toggle('active');
 }
@@ -54,5 +69,4 @@ function scrollToContacts() {
 }
 let copiedText = document.querySelectorAll('#trythis');
 copiedText.forEach((elem) => elem.addEventListener('click', () => copyText(elem)));
-
-
+console.log(colorLogo);
